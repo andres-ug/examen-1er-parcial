b1f8d33244238fa70d11b2ea3b553044578233b4
Andres Suarez <andres.suarez@fastdevelopments.net>
2023-11-23 18:54:48 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'examen-1er-parcial',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
